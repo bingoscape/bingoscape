4ff568a930021c9b9fa4b51b63dadd437c58d20b
Sebastian Schubert <sschubert932@gmail.com>
2024-10-03 16:03:41 +0200

    Added loading skeleton

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export function createArray(length: number) {
+  return Array.from({ length }, (_, index) => index)
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
